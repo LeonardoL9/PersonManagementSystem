81aa1d880015118b577ec0b8fb087332ba8179d4
LeonardoL9 <114910367+LeonardoL9@users.noreply.github.com>
2023-12-01 00:29:21 +0800

    Add files via upload

---
+++ PersonnelService.java
@@ -0,0 +1,7 @@
+package com.maker.lp.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.maker.lp.entity.Personnel;
+
+public interface PersonnelService extends IService<Personnel> {
+}

cb2a8181d0856be665a1feb6da87c1c3b7b30152
LeonardoL9 <114910367+LeonardoL9@users.noreply.github.com>
2023-11-30 13:27:42 +0800

    Add files via upload

---
+++ PersonMapper.java
@@ -0,0 +1,9 @@
+package com.maker.lp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.maker.lp.entity.Person;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonMapper extends BaseMapper<Person> {
+}
---
+++ PersonnelMapper.java
@@ -0,0 +1,9 @@
+package com.maker.lp.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.maker.lp.entity.Personnel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonnelMapper extends BaseMapper<Personnel> {
+}
